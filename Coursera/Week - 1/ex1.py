136befea60399daa466f001e5c7764ab23513633
lightecho037 <30530802+lightecho037@users.noreply.github.com>
2018-05-30 06:58:33 -0500

    convertion Miles to Feet 13

---
+++ Coursera/Week - 1/ex1.py
@@ -0,0 +1,11 @@
+
+#!/usr/local/bin/python3.6
+mile_var = 5280
+print (f"This scrip converts Miles to feets.")
+cal_miles = int(input(f"Enter the number of miles: "))
+convertion = mile_var * cal_miles
+print (f"This is the total Feef in {cal_miles} miles {convertion} ")
+~                                                                        
+
+
+
